bee2f5a1b0032d5c8a812ffa85504fc9208bf6e2
EnzoWu <75583251+EnzoWu479@users.noreply.github.com>
2024-03-08 10:12:16 -0300

    fix: bcrypt

--- frontend/src/server/controllers/AccountController.ts
+++ frontend/src/server/controllers/AccountController.ts
@@ -1,6 +1,10 @@
-import { PrismaClient } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next";
-import container from "../lib/inversify/container";
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+import container from '../lib/inversify/container';
+import { sign } from 'jsonwebtoken';
+import { serialize } from 'cookie';
+import { COOKIES_NAME } from '@/config/constants';
+import { compare } from 'bcrypt';
 
 export class AccountController {
   private prisma: PrismaClient;
@@ -12,7 +16,7 @@ export class AccountController {
   public static async login(req: NextApiRequest, res: NextApiResponse) {
     const { email, password } = req.body;
     const account = await prisma?.account.findUnique({
-      where: { email },
+      where: { email }
     });
 
     if (!account) {
@@ -25,27 +29,33 @@ export class AccountController {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    const token = sign({ role: account.role }, process.env.JWT_SECRET || '');
+    const token = sign({ role: account.roles }, process.env.JWT_SECRET || '');
 
-    res.setHeader('Set-Cookie', serialize(COOKIES_NAME.TOKEN, token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 60 * 60 * 24 * 7, // 1 week
-      path: '/',
-    }));
+    res.setHeader(
+      'Set-Cookie',
+      serialize(COOKIES_NAME.TOKEN, token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 60 * 60 * 24 * 7, // 1 week
+        path: '/'
+      })
+    );
 
     res.status(200).json({ message: 'Logged in' });
   }
 
   public static async logout(req: NextApiRequest, res: NextApiResponse) {
-    res.setHeader('Set-Cookie', serialize(COOKIES_NAME.TOKEN, '', {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      expires: new Date(0),
-      path: '/',
-    }));
+    res.setHeader(
+      'Set-Cookie',
+      serialize(COOKIES_NAME.TOKEN, '', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        expires: new Date(0),
+        path: '/'
+      })
+    );
 
     res.status(200).json({ message: 'Logged out' });
   }
