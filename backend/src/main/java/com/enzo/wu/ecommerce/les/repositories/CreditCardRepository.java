0bb260292063236016e75a06a203709844ec30af
EnzoWu <75583251+EnzoWu479@users.noreply.github.com>
2024-03-12 14:26:14 -0300

    Delete backend directory

--- backend/src/main/java/com/enzo/wu/ecommerce/les/repositories/CreditCardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enzo.wu.ecommerce.les.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.enzo.wu.ecommerce.les.models.CreditCard.CreditCard;
-
-public interface  CreditCardRepository extends JpaRepository<CreditCard, Long> {
-    
-}
