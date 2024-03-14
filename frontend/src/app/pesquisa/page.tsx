d9caed3a9a52e73341b61743a5af730fe4247869
EnzoWu <75583251+EnzoWu479@users.noreply.github.com>
2024-03-14 10:33:10 -0300

    fix: search

--- frontend/src/app/pesquisa/page.tsx
+++ frontend/src/app/pesquisa/page.tsx
@@ -1,8 +1,11 @@
 import { ClientNavigationMenu } from '@/components/client/client-navigation-menu';
 import { ProductCard } from '@/components/client/product-card';
+import { ProductSection } from '@/components/product-section';
 import { Card, CardContent } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
 import { ClientLayout } from '@/layouts/client-layout';
 import Image from 'next/image';
+import { Suspense } from 'react';
 
 interface SearchParams {
   params: {};
@@ -16,8 +19,21 @@ export default function Search(params: SearchParams) {
     <ClientLayout>
       <div className="mx-auto mt-8 max-w-7xl px-4 sm:px-6 lg:px-8">
         <h1 className="text-lg">Pesquisando por: {params.searchParams.q}</h1>
-        <div className="mt-4 grid px-8 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 lg:grid-cols-4">
-          <ProductCard />
+        <div className="mt-4 grid gap-4 px-8 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 lg:grid-cols-4">
+          <Suspense
+            fallback={
+              <>
+                <Skeleton className="h-80 w-full" />
+                <Skeleton className="h-80 w-full" />
+                <Skeleton className="h-80 w-full" />
+                <Skeleton className="h-80 w-full" />
+                <Skeleton className="h-80 w-full" />
+                <Skeleton className="h-80 w-full" />
+              </>
+            }
+          >
+            <ProductSection />
+          </Suspense>
         </div>
       </div>
     </ClientLayout>
