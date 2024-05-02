e7d279ac304f36bc3d0e18697f236e9e339a2649
EnzoWu479 <enwu2014@hotmail.com>
2024-05-02 17:11:50 -0300

    fix: delete useless code

--- frontend/src/pages/api/address/delivery/index.ts
+++ frontend/src/pages/api/address/delivery/index.ts
@@ -1,15 +1,7 @@
 import { AddressController } from '@/server/controllers/AddressController';
-import { authorizationMiddleware } from '@/server/middlewares/authorizationMiddleware';
 import { SingletonClass } from '@/server/singleton/SingletonClass';
-import { AccountRoles } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { createRouter } from 'next-connect';
-
-export const config = {
-  api: {
-    externalResolver: true
-  }
-};
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
 
--- frontend/src/pages/api/cart/index.ts
+++ frontend/src/pages/api/cart/index.ts
@@ -1,24 +1,25 @@
 import { CartController } from '@/server/controllers/CartController';
-import { authorizationMiddleware } from '@/server/middlewares/authorizationMiddleware';
 import { SingletonClass } from '@/server/singleton/SingletonClass';
-import { AccountRoles } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { createRouter } from 'next-connect';
 
-export const config = {
-  api: {
-    externalResolver: true
-  }
-};
+// export const config = {
+//   api: {
+//     externalResolver: true
+//   }
+// };
 
+console.log("oiie");
 const router = createRouter<NextApiRequest, NextApiResponse>();
 
 const cartController = SingletonClass.getInstance(CartController);
+
 // Publica
 router
   // .use(authorizationMiddleware([AccountRoles.USER]))
   .get(cartController.getCurrentCart)
   .post(cartController.addProductToCart)
   .put(cartController.updateProductAmount);
+  // .get((req, res) => res.status(200).json({oi: "a"}))
 
 export default router.handler();
