565e205c920b5ba01686389d0938a82ea3ebda13
EnzoWu <75583251+EnzoWu479@users.noreply.github.com>
2024-06-23 15:21:55 -0300

    fix: scroll to bottom on receive message

--- frontend/src/features/chat/components/chat.tsx
+++ frontend/src/features/chat/components/chat.tsx
@@ -5,7 +5,7 @@ import { SendHorizonal } from 'lucide-react';
 import { Textarea } from '@/components/ui/textarea';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useChat } from '../hooks/useChat';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { AiMessage } from './ai-message';
 import { MyMessage } from './my-message';
 import { LoadingMessage } from './loading-message';
@@ -16,12 +16,22 @@ interface Props {
 
 export const Chat = ({ isOpen }: Props) => {
   const [message, setMessage] = useState('');
+  const refScroll = useRef<HTMLDivElement>(null);
   const { isPending, messages, sendMessage } = useChat();
+
+  useEffect(() => {
+    if (refScroll.current) {
+      refScroll.current.scrollTop = refScroll.current.scrollHeight;
+    }
+  }, [messages])
   return (
     <div className={cn(`grid grid-rows-[0fr]`, isOpen && 'grid-rows-1')}>
       <div className="overflow-hidden">
         <div className="flex h-80 w-80 flex-col justify-between gap-2 border border-t-0 bg-white p-4">
-          <div className=" flex w-full flex-col gap-2 overflow-auto px-2">
+          <div
+            className=" flex w-full flex-col gap-2 overflow-auto px-2"
+            ref={refScroll}
+          >
             {messages.map((message, index) => (
               <>
                 {message.isUser ? (
