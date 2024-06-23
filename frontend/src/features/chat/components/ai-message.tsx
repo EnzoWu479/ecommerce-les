6d67e46daffa8bb47c1cae8b14f0d5fa854c4178
EnzoWu <75583251+EnzoWu479@users.noreply.github.com>
2024-06-23 15:21:26 -0300

    fix: search button on chat

--- frontend/src/features/chat/components/ai-message.tsx
+++ frontend/src/features/chat/components/ai-message.tsx
@@ -1,21 +1,33 @@
 import { ChatMessage } from '@/types/chat';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { LittleProductCard } from '@/components/client/little-product-card';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 export const AiMessage = ({ message }: { message: ChatMessage }) => {
   return (
-    <div>
-      <div className="flex gap-2">
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-        <div className="mr-auto w-fit text-wrap rounded-md rounded-tl-none border bg-slate-100 px-4 py-2 text-sm text-slate-950">
+    <div className="flex gap-2">
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <div className="flex flex-col">
+        <div
+          className={cn(
+            'mr-auto w-fit text-wrap rounded-md rounded-tl-none border bg-slate-100 px-4 py-2 text-sm text-slate-950',
+            !!message.bookName && `rounded-none rounded-tr-md`
+          )}
+        >
           {message.message}
         </div>
-      </div>
-      <div className="mt-2">
-        {message.book && <LittleProductCard product={message?.book} />}
+        {message.bookName && (
+          <Link
+            href={`/pesquisa?q=${message.bookName}`}
+            className="w-full rounded-b-md bg-slate-950 py-2 text-center text-sm text-white"
+          >
+            Pesquisar livro
+          </Link>
+        )}
       </div>
     </div>
   );
